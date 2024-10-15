d45b770ca10cb50180d4ae88585b72982d88f25d
phmelosilva <pedrodsm1819@gmail.com>
2024-10-14 22:48:47 -0300

    config router, web vitals

---
+++ src/MinhasTarefas.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MinhasTarefas() {
+  return (
+    <div>
+      <h1>Minhas Tarefas</h1>
+      <p>Esta é a página onde minhas tarefas serão listadas.</p>
+    </div>
+  );
+}
+
+export default MinhasTarefas;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import reportWebVitals from './reportWebVitals.js';
+import React from 'react'; 
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MinhasTarefas from './MinhasTarefas';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/minhasTarefas" element={<MinhasTarefas />} /> 
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
